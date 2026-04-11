cdefd826d31d6fb0a12ea49e1a8d93fa81fac5da
PragatiNigam29 <nigampragati1029@gmail.com>
2026-04-11 12:13:25 +0530

    Add home view for Mail Tracker

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Mail Tracker is running 🚀")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tracker.urls')),
+    path('', home),
+    path('tracker/', include('tracker.urls')),
 ]
+
+    
