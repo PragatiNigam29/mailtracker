5995d98fd4397f7aebb4311533d931e54b573169
PragatiNigam29 <nigampragati1029@gmail.com>
2026-04-11 12:48:34 +0530

    Add URL path for sending email

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('track/<uuid:tracking_id>/', views.track_email, name='track_email'),
+    path('send-email/', views.send_email, name='send_email'),
 ]
