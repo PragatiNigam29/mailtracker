0340420cf3ba1dd1f4650884c1887d98daf06f50
PragatiNigam29 <nigampragati1029@gmail.com>
2026-04-11 14:12:44 +0530

    Remove SMTP email backend configuration
    Removed email configuration settings for SMTP.

--- config/settings.py
+++ config/settings.py
@@ -98,14 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'nigampragati1029@gmail.com'
-EMAIL_HOST_PASSWORD = 'xbtn ufck ngnf cbvz'
 
 
 # Internationalization
